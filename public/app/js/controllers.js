0f7fffe15a4ad46ee8aa04a4134f8b9c9a8f12d5
Александр <ihumster@ihumster.ru>
2018-01-02 14:24:45 +0300

    update front app

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -1,5 +1,12 @@
 angular.module('SUClan')
     .controller('homeStateController', ['$scope', 'services', '$state', function ($scope, services, $state) {
+        $scope.settings = {
+            header: {
+                model: null,
+                isShownSearch: false,
+                placeholder: undefined
+            }
+        }
         $scope.clans = services.clans;
         $scope.isVisibleItems = false;
         $scope.toggleMenuItems = function (event) {
@@ -10,12 +17,26 @@ angular.module('SUClan')
     }])
 
     .controller('suHeaderController', ['$scope', 'services', '$state', function ($scope, services, $state) {
-
+        $scope.isShownSearch = $scope.headerConfig.isShownSearch;
+        $scope.placeholder = $scope.headerConfig.placeholder;
+        $state.srModel = $scope.headerConfig.model;
+        $scope.headerStyle = $state.is('home') ? 'appHeaderContainer isHomeState' : 'appHeaderContainer'
+        $scope.appHeaderContainer = true;
+        console.log($state.is('home'));
+    
     }])
 
     .controller('clanStatistic', ['$scope', 'services', '$state', '$stateParams', 'orderByFilter', function ($scope, services, $state, $stateParams, orderBy) {
+        $scope.settings = {
+            header: {
+                model: '',
+                isShownSearch: true,
+                placeholder: 'search member'
+            }
+        }
+ 
         var clanList;
-        var membersList = services.getData($stateParams.clanNumber, function (response, isLoading) {
+        var membersList = services.getData($stateParams.clanNumber-1, function (response, isLoading) {
             clanList = response;
             $scope.clanList = response;
             $scope.isLoading = isLoading;
--- public/app/js/routes.js
+++ public/app/js/routes.js
@@ -1,52 +1,24 @@
 angular.module('SUClan', ['ui.router'])
    .config(function($stateProvider, $urlRouterProvider){
+
       var homeState = {
          name: 'home',
          url: '/home',
          templateUrl: './app/templates/home.html',
          controller: 'homeStateController'
       };
-      var clanStatisticV1 = {
-         name: 'clanStatisticV1',
-         url: '/clanStatisticV1',
-         params: {
-            clanNumber: null
-         },
-         templateUrl: '/app/templates/clanStatisctic.html',
-         controller: 'clanStatistic'
-      };
-      var clanStatisticV2 = {
-         name: 'clanStatisticV2',
-         url: '/clanStatisticV2',
-         params: {
-            clanNumber: null
-         },
-         templateUrl: '/app/templates/clanStatisctic.html',
-         controller: 'clanStatistic'
-      };
-      var clanStatisticV3 = {
-         name: 'clanStatisticV3',
-         url: '/clanStatisticV3',
-         params: {
-            clanNumber: null
-         },
-         templateUrl: '/app/templates/clanStatisctic.html',
-         controller: 'clanStatistic'
-      };
-      var clanStatisticV4 = {
-         name: 'clanStatisticV4',
-         url: '/clanStatisticV4',
-         params: {
-            clanNumber: null
-         },
-         templateUrl: '/app/templates/clanStatisctic.html',
-         controller: 'clanStatistic'
-      };
 
-      $stateProvider.state(clanStatisticV1);
-      $stateProvider.state(clanStatisticV2);
-      $stateProvider.state(clanStatisticV3);
-      $stateProvider.state(clanStatisticV4);
+      var clanStatistic = {
+         name: 'clanStatistic',
+         url: '/clanStatistic/v{clanNumber}',
+         params: {
+            clanNumber: null
+         },
+         templateUrl: '/app/templates/clanStatisctic.html',
+         controller: 'clanStatistic'
+      };
+      
+      $stateProvider.state(clanStatistic);
       $stateProvider.state(homeState);
       $urlRouterProvider.otherwise('/home');
    });
