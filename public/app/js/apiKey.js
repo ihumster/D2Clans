6f63ef29f7bff818378c9c70c09aa188d6c59b1f
Aliaksandr Limasau <alexandr.limasov@gmail.com>
2018-02-18 22:45:52 +0300

    api key

--- public/app/js/apiKey.js
+++ public/app/js/apiKey.js
@@ -1,4 +1,4 @@
-var apiKey = '8d7f75b5c93c4a61a55e5c072bc8a1a4';
+var apiKey = '4388716a85d64f39b616374c195f96b2';
 var clientId = 23137;
 var authUrl = 'https://www.bungie.net/ru/OAuth/Authorize';
 var tokenUrld = 'https://www.bungie.net/Platform/App/OAuth/token/';
