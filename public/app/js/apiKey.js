142de9bb4d791a5e83dc88cb393717baa5dea46f
Aliaksandr Limasau <Aliaksandr_Limasau@epam.com>
2018-02-23 18:02:01 +0300

    dev - key

--- public/app/js/apiKey.js
+++ public/app/js/apiKey.js
@@ -1,4 +1,4 @@
-var apiKey = '4388716a85d64f39b616374c195f96b2';
+var apiKey = '6b1b4758f7ff430d9bbc2b05c7f38bcc';
 var clientId = 23137;
 var authUrl = 'https://www.bungie.net/ru/OAuth/Authorize';
 var tokenUrld = 'https://www.bungie.net/Platform/App/OAuth/token/';
