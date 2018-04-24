35b6864feac2e9655ba6a3cdecc32c72f5726180
Aliaksandr Limasau <Aliaksandr_Limasau@epam.com>
2018-04-24 10:31:10 +0300

    en gas been removed

--- routes/index.js
+++ routes/index.js
@@ -24,7 +24,7 @@ router.get('/weeklyResetView', function (req, res, next) {
             'Content-type': 'html'
         });
         res.end(`<!DOCTYPE html>
-            <html lang="en">
+            <html>
             <head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
