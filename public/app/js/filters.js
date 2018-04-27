8b498879c29765ba1070e3981c190a0aadd159d6
Aliaksandr Limasau <Aliaksandr_Limasau@epam.com>
2018-04-27 12:13:32 +0300

    CLAN LIST IMPROVEMENT

--- public/app/js/filters.js
+++ public/app/js/filters.js
@@ -1,6 +1,10 @@
 angular.module('SUClan')
     .filter('timeFilter', function(){
         return function(item) {
-            return item.replace('T', ' ').replace('Z','');
+            
+            var outputValue = (new Date() - new Date(item))/86400000;
+            var daysAgo = outputValue.toFixed(1);
+            
+            return daysAgo;
         }
     })
